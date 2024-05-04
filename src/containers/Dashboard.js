f3b27540f658b9572fe0e28ea7c1dd093412f314
timotheG <gonin.timothe@gmail.com>
2024-05-04 16:56:20 +0200

    🚧 enabling current bill edition + styles

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,25 +86,32 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
-      bills.forEach(b => {
-        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
-      })
-      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
+    const currentBill = e.currentTarget
+    bills.forEach(b => {
+      $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+    })
+    $(currentBill).css({ background: '#2A2B35' })
+    $(currentBill).addClass('active');
+    // if (this.counter === undefined || this.id !== bill.id) this.counter = 0
+    // if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+    // if (this.counter % 2 === 0) {
+    
+    //   bills.forEach(b => {
+    //     $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+    //   })
+    //   $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
-    } else {
-      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+    //   this.counter ++
+    // } else {
+    //   $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
-      $('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      `)
-      $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
-    }
+    //   $('.dashboard-right-container div').html(`
+    //     <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+    //   `)
+    //   $('.vertical-navbar').css({ height: '120vh' })
+    //   this.counter ++
+    // }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))

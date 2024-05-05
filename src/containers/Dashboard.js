38dfe4cb9d9fdf7012bee87ad5b96db5b4f0eb42
timotheG <gonin.timothe@gmail.com>
2024-05-05 14:23:19 +0200

    🐛 attribution of the event to the correct element

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,32 +86,32 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    const currentBill = e.currentTarget
-    bills.forEach(b => {
-      $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
-    })
-    $(currentBill).css({ background: '#2A2B35' })
-    $(currentBill).addClass('active');
-    // if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    // if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    // if (this.counter % 2 === 0) {
+    // const currentBill = e.currentTarget
+    // bills.forEach(b => {
+    //   $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+    // })
+    // $(currentBill).css({ background: '#2A2B35' })
+    // $(currentBill).addClass('active');
+    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
+    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+    if (this.counter % 2 === 0) {
     
-    //   bills.forEach(b => {
-    //     $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
-    //   })
-    //   $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
+      bills.forEach(b => {
+        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+      })
+      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-    //   this.counter ++
-    // } else {
-    //   $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+      this.counter ++
+    } else {
+      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
-    //   $('.dashboard-right-container div').html(`
-    //     <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-    //   `)
-    //   $('.vertical-navbar').css({ height: '120vh' })
-    //   this.counter ++
-    // }
+      $('.dashboard-right-container div').html(`
+        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+      `)
+      $('.vertical-navbar').css({ height: '120vh' })
+      this.counter ++
+    }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -153,7 +153,7 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#status-bills-container${this.index} #open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills

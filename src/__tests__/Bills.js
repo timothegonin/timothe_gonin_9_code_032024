2b6cc4dd43b0611622ead69811a9186876cf35bf
timotheG <gonin.timothe@gmail.com>
2024-05-09 10:28:56 +0200

    ✅ adding expect mention view/Bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -25,8 +25,8 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      //to-do write expect expression (OK)
+      expect(windowIcon.classList).toContain('active-icon')
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })

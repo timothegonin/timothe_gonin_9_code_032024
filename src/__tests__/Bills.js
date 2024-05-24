f6b6860224dafd4f3493083cabe2c6ee9ac5220b
timotheG <gonin.timothe@gmail.com>
2024-05-24 10:35:14 +0200

    ✅ 🚧 new bill form,   handleClickNewBill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,15 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {getByTestId, screen, waitFor} from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -34,6 +36,22 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+  describe("When I am on Bills page and clicking on 'New bill' button" , () => {
+    test("Then, 'New Bill' form should be rendered", () => {
+    
+      
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      const button = screen.getByTestId('btn-new-bill')
+      
+      expect(getByTestId(document.body, 'btn-new-bill')).toBeTruthy()
+      userEvent.click(button)
+      expect(getByTestId(document.body, 'form-new-bill')).toBeTruthy()
     })
   })
 })

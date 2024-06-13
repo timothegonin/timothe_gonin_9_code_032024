6a5504b77d130bff56908e61b60205fa254cf5ea
timotheG <gonin.timothe@gmail.com>
2024-06-13 08:17:24 +0200

    ✅ 500 error test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -129,9 +129,22 @@ describe("Given I am a user connected as Employee", () => {
             return Promise.reject(new Error("Erreur 404"))
           }
         }})
-      window.onNavigate(ROUTES_PATH.Dashboard)
+      window.onNavigate(ROUTES_PATH.Bills)
       await new Promise(process.nextTick);
       const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("fetches messages from API and fails with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () => {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }
+      })
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
       expect(message).toBeTruthy()
     })
   })

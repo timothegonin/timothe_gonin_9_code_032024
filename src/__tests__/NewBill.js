3c34790a9db7427a315543f72d0ecc1b02a72102
timotheG <gonin.timothe@gmail.com>
2024-06-13 16:08:30 +0200

    ✅ updating NewBill test environnement

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,13 +5,28 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES_PATH} from "../constants/routes.js";
+
+
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then the form elements should be present", () => {
+    test("Then the form elements should be present", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
       const html = NewBillUI()
       document.body.innerHTML = html
+
       //to-do write assertion
       expect(screen.getByTestId("form-new-bill")).toBeTruthy();
       expect(screen.getByTestId("expense-type")).toBeTruthy();
